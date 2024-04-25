627532a446fa7a316c1c19a5e8126fac4ea10c4c
Aasif Raza <aasif.raza@dedalus.eu>
2024-04-25 17:08:07 +0530

    first commit

---
+++ src/test/java/com/raza/blog/JavaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raza.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
